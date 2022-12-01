3eaad5eb29edf8e16525146dd507f456ac79c339
autumnWindigo <fogeyfun@gmail.com>
2022-12-01 14:52:50 -0500

    generates one int

--- day-1/calorieTracker.asm
+++ day-1/calorieTracker.asm
@@ -10,9 +10,10 @@ currentSum: db      0           ;current sum of calories
     ;; String vars
 lineSize:   db      0           ;size of line
 filePtr:    db      0           ;pointer for file
+linePtr:    db      0           ;pointer of current line
 
 section     .bss
-lineCont:   resb 255            ;Reserved bytes for lines
+lineCont:   resb 10            ;Reserved bytes for lines
 fileCont:   resb 10408          ;Reserved bytes for file
 
 ; ---------------------------------
@@ -23,25 +24,28 @@ global      _start
 _start:
     call    openFile            ;Get file contents into reserved memory
     mov     ecx, fileCont       ;put pointer for fileCont into ecx
+    xor     eax, eax                ;inbetween iterations
 
 getLength:
-.iterateChar:
-    add     ecx, 1              ;iterate forward one char
+    jmp    .buildInt
 
 .endLineCheck:
+    add     ecx, 1              ;iterate forward one char
+    mov     edx, [linePtr]      ;VV
+    add     edx, 1              ;linePtr += 1
+    mov     [linePtr], edx      ;^^
     cmp     [ecx], byte 0Ah     ;if 0 (newLine) is the character
-    jnz     .iterateChar        ;if they aren't equal, iterate more
+    jnz     getLength           ;jump to getLength
 
     mov     [filePtr], ecx      ;set ptr to end of current line
-    sub     ecx, fileCont       ;get length of first String
-
-    ;; ecx is length
-    ;; eax is start of line
-.iterateLine:
-    mov     eax, fileCont
-    mov     esi, [eax+4]
 
 
+.buildInt:
+    mov     bl, byte [ecx]     ;put current byte into bl
+    sub     bl, '0'            ;convert ASCII to int
+    imul    eax, 10
+    add     eax, ebx
+    jmp .endLineCheck
 ;--------------------------------
 ;Exits gracefully
 quit:
@@ -69,62 +73,3 @@ openFile:
 
     mov     eax, fileCont
     ret
-
-;;; Functions -----------------------------
-;------------------------------------------
-; int slen(String message)
-; String length calculation function
-slen:
-    push    rbx
-    mov     rbx, rax
-
-nextchar:
-    cmp     byte [rax], 0
-    jz      finished
-    inc     rax
-    jmp     nextchar
-
-finished:
-    sub     rax, rbx
-    pop     rbx
-    ret
-
-
-;------------------------------------------
-; void sprint(String message)
-; String printing function
-; Takes input in rax
-sprint:
-    push    rdx
-    push    rcx
-    push    rbx
-    push    rax
-    call    slen
-
-    mov     rdx, rax
-    pop     rax
-
-    mov     rcx, rax
-    mov     rbx, 1
-    mov     rax, 4
-    int     80h
-
-    pop     rbx
-    pop     rcx
-    pop     rdx
-    ret
-
-;------------------------------------------
-; void sprintLF(String message)
-; String printing with line feed function
-sprintLF:
-    call    sprint
-
-    push    rax         ; push eax onto the stack to preserve it while we use the eax register in this function
-    mov     rax, 0Ah    ; move 0Ah into eax - 0Ah is the ascii character for a linefeed
-    push    rax         ; push the linefeed onto the stack so we can get the address
-    mov     rax, rsp    ; move the address of the current stack pointer into eax for sprint
-    call    sprint      ; call our sprint function
-    pop     rax         ; remove our linefeed character from the stack
-    pop     rax         ; restore the original value of eax before our function was called
-    ret                 ; return to our program
