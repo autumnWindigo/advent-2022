21ffa45158f4021c42a30075424fdcc40547eabc
autumnWindigo <fogeyfun@gmail.com>
2022-12-05 16:18:54 -0500

    day-3 solution part 1

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,16 +1,55 @@
 use std::io::Read;
 use std::fs::File;
 use std::path::Path;
+use std::collections::BTreeSet;
 
 fn main() {
     let mut rucksacks = get_file("src/input.txt");
-    let test: u8 = match rucksacks.pop() {
-        Some(item) => item,
-        None => 0
-    };
-    let rucksacks_iter = rucksacks.into_iter();
+    let mut optional = rucksacks.pop();
+    let mut total: u32 = 0;
+    
+    let mut line_length;
+    let mut line_vec: Vec<u8> = Vec::new();
+    
+    let mut half_one: BTreeSet<u8> = BTreeSet::new();
+    let mut half_two: BTreeSet<u8> = BTreeSet::new();
 
-
+    
+    while !rucksacks.is_empty() {
+        optional = rucksacks.pop(); //pops new line
+        //Pop from Vec until (10) newLine
+        while let Some(item) = optional {
+            if item == 10 { // new line, stop reading
+                optional = None;
+            }   else {
+                line_vec.push(item);
+                optional = rucksacks.pop();
+            }    
+        } //End of while let
+        line_length = line_vec.len();
+        for value in 0 .. line_length {
+            if value <= line_length/2-1 {
+                match line_vec.pop(){
+                   Some(value) => half_one.insert(value),
+                   None => false
+                };
+            }   else {
+                match line_vec.pop() {
+                    Some(value) => half_two.insert(value),
+                    None => false
+                };
+            }
+        }
+        let intersection: Vec<_> = half_one.intersection(&half_two).cloned().collect();
+        if intersection[0] > 96 {
+            total += (intersection[0]-96) as u32;
+        } else {
+            total += (intersection[0]-38) as u32;
+        }
+        half_one.clear();
+        half_two.clear();
+    }
+    println!("total: {}", total);
 }
 
 /*
