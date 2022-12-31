703f6d4efa8a96a5f1fa96a833c6dd4e2b388961
autumnWindigo <fogeyfun@gmail.com>
2022-12-31 12:48:13 -0500

    added day 6 solution 1

---
+++ day-6/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::Read;
+use std::collections::VecDeque;
+
+fn main() -> std::io::Result<()> {
+    let mut f = File::open("src/input.txt")?;
+    let mut buffer = Vec::new();
+    let mut marker = VecDeque::<u8>::new();
+    let mut marker_check: Vec::<u8>;
+    let mut result: u32 = 0;
+    let mut char_check: u8;
+    f.read_to_end(&mut buffer)?;
+
+    for &char in &buffer {
+        //Insert Char
+        result += 1;
+        marker.push_back(char);
+        marker_check = marker.clone().into();
+        //Remove Dups
+        marker_check.sort();
+        marker_check.dedup();
+
+        //check size
+        if marker_check.len() == 4 {
+            break;
+        }
+        //Remove Old
+        if marker.len() >= 4 {
+            char_check = marker.pop_front().unwrap();
+            marker_check.retain(|&x| x != char_check);
+        }
+    }
+
+    println!("Answer: {}", result);
+    Ok(())
+}
+
